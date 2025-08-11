b409dd327671e2cc0b66c4dee704405b233acdd3
sandeep chauhan <sandeepchauhan893@gmail.com>
2025-08-12 01:50:41 +0530

    update: word replaced v2

--- src/main/kotlin/DSA/CodChef/CodeChef.kt
+++ src/main/kotlin/DSA/CodChef/CodeChef.kt
@@ -1,5 +1,26 @@
-package DSA.CodChef
-
 fun main() {
-    print("Hello CodeChef")
+    val t = readLine()!!.toInt()
+    repeat(t) {
+        val n = readLine()!!.toInt()
+        val p = readLine()!!.split(" ").map { it.toInt() }
+
+        var pos1 = -1
+        var posN = -1
+
+        for (i in 0 until n) {
+            if (p[i] == 1) {
+                pos1 = i
+            }
+            if (p[i] == n) {
+                posN = i
+            }
+        }
+
+        var ans = pos1 + (n - 1 - posN)
+        if (pos1 > posN) {
+            ans--
+        }
+
+        println(ans)
+    }
 }
